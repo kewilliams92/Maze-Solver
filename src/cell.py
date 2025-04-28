c496b4e6caa7f90dc2fa16fa6659931224478c73
kw92 <kewilliamsdev@gmail.com>
2025-04-28 14:48:08 -0400

    Finished putting together cell

---
+++ src/cell.py
@@ -0,0 +1,35 @@
+from window import Line, Point
+
+
+class Cell:
+    def __init__(self, win):
+        """The data members used to create the cell."""
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self.__win = win
+
+    def draw(self, x1, x2, y1, y2):
+        # The cell needs to have coordinates in order to draw each wall.
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        """We draw a line based on if the cell has walls using the Line and Point classes from our window.py."""
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.__win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.__win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.__win.draw_line(line)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
+from cell import Cell
 from window import Window
 
 
 def main():
     win = Window(800, 600)
+    # create a cell
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(100, 100, 200, 200)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(200, 100, 300, 200)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(300, 100, 400, 200)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(400, 200, 500, 100)
+
     win.wait_for_close()
 
 
